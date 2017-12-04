f23e9aec7ca628514db90631123cda80c42e763e
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-12-03 20:42:37 -0800

    added for loops to check answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,72 @@
 // create object for the questions and answers
 
-var qa1 = [{
+var questions = [{
     q: 'He was called "The King of Pop"', 
     choices: ["Michael Jackson", "Prince", "Justin Bieber", "Justin Timberlake"],
-    a: "Michael Jackson"},
+    answer: "Michael Jackson"},
     {
         q: 'Chris Hemsworth plays this superhero',
 choices: ["Batman", "Captain America", "Thor", "Iron Man"],
-a: "Thor"
+answer: "Thor"
 },
 {
     q: 'This movie was top at the box office in 2015',
     choices: ["Mad Max: Fury Road", "The Revenant", "Star Wars Ep. VII: The Force Awakens", "Jurassic World"],
-    a: "Star Wars Ep. VII: The Force Awakens"
+    answer: "Star Wars Ep. VII: The Force Awakens"
 },
 {
     q: 'This remake of a 1984 movie stars a female cast including Melissa McCarthy and Kristen Wiig',
     choices: ["The Fog", "Ghostbusters", "Prom Night", "It"],
-    a: "Ghostbusters"
+    answer: "Ghostbusters"
 },
 {
     q: 'They won the 2016 Superbowl',
     choices: ["New England Patriots", "Pittsburgh Steelers", "Baltimore Ravens", "Denver Broncos"],
-    a: "Denver Broncos"
+    answer: "Denver Broncos"
 },
 {
     q: 'This artist is responsible for the hit "One Dance"',
     choices: ["Drake", "Rhianna", "Calvin Harris", "One Direction"],
-    a: "Drake"
+    answer: "Drake"
 },
 {
     q: "This TV series featuring the Tanner Family which was originally created in the 1980's made its way back to TV this past year with a new name",
     choices: ["Fresh Prince of Bel Air", "Family Matters", "Step By Step", "Full House"],
-    a: "Full House"
+    answer: "Full House"
 },
 {
     q: "He is Batman's sidekick",
     choices: ["Alfred", "The Joker", "Catwoman", "Robin"],
-    a: "Robin"
+    answer: "Robin"
 },
 {
     q: "This hit HBO show was based on the books written by George R.R. Martin",
     choices: ["The Wire", "The Sopranos", "Game of Thrones", "Oz"],
-    a: "Game of Thrones"
+    answer: "Game of Thrones"
 },
 {
     q: "The New York Yankees beat this team to win the 1998 World Series",
     choices: ["San Diego Padres", "Atlanta Braves", "Oakland Athletics", "Cleveland Indians"],
-    a: "San Diego Padres"
+    answer: "San Diego Padres"
 }];
 
-
-var choice = $("")
-//console.log(questions, answers);
 
 $(document).ready(function() {
 // when the user clicks submit
 $("#btn").on("click", function () {
-    var chosen = "";
-    if (document.textBox.group1[0].checked){
-        chosen = document.quiz.question1[0].value;
+    //variable for correct answers
+  var amountCorrect = 0;          
+  for(var i = 1; i <= 10; i++) {
+    var radios = document.getElementsByName('group'+i);
+    for(var j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      if(radio.value == "correct" && radio.checked) {
+        amountCorrect++;
+      }
     }
-    else if (document.quiz.question1[1].checked){
-        chosen =  document.quiz.question1[1].value;
+   }                   
+      alert("Correct Responses: " + amountCorrect);
     }
-    else{
-        chosen = document.quiz.question1[2].value;
-    }
-}
 
 
     // keep track of amount correct
